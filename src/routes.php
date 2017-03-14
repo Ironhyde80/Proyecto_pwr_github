89c16f0fc489cf43364fcc975e0441dab9473c94
Ironhyde80 <garcialopezgustavo@gmail.com>
2017-03-14 15:48:14 +0100

    se crean las rutas

--- src/routes.php
+++ src/routes.php
@@ -14,14 +14,22 @@ $app->get('/acercade', function ($request, $response, $args) {
     $this->logger->info("Vista acerca de");
 
     // Render index view
-    return $this->renderer->render($response, 'acercade.html', $args);
+    return $this->renderer->render($response, 'acercade.php', $args);
 });
 
 $app->get('/upload', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Vista acerca de");
+    $this->logger->info("Aqui se haran los uploads");
 
     // Render index view
-    return $this->renderer->render($response, 'upload.html', $args);
+    return $this->renderer->render($response, 'upload.php', $args);
+});
+
+$app->get('/login', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Aqui se haran los login");
+
+    // Render index view
+    return $this->renderer->render($response, 'login.php', $args);
 });
 
