18a8fb398499233d9254a9dbc853b8e961ba3638
Ironhyde80 <garcialopezgustavo@gmail.com>
2017-03-14 15:44:50 +0100

    Se ha quitado la carpeta que contiene las fotos y logos por seguridad

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,27 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.html', $args);
 });
+
+$app->get('/acercade', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Vista acerca de");
+
+    // Render index view
+    return $this->renderer->render($response, 'acercade.html', $args);
+});
+
+$app->get('/upload', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Vista acerca de");
+
+    // Render index view
+    return $this->renderer->render($response, 'upload.html', $args);
+});
+
