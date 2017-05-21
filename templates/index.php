448a2024a3c91dfc04ad35f7c2254f26c69c1151
aypach3r <atpacheco@outlook.com>
2017-05-21 18:24:48 +0200

    corrigiendo errores

--- templates/index.php
+++ templates/index.php
@@ -2,24 +2,24 @@
 {% block title %}Inicio{% endblock %}
 {% block body %}
 	<table class="table table-hover">
-	<thead>
-		<tr>
-			<th></th>
-			<th>NOMBRE</th>
-			<th>APELLIDOS</th>
-			<th></th>
-		</tr>
-	</thead>
-	<tbody>
+		<thead>
+			<tr>
+				<th></th>
+				<th>NOMBRE</th>
+				<th>APELLIDOS</th>
+				<th><input type="submit" value="Aplicar licencia" class="btn btn-success" id="agregarLicencia"></th>
+				<th><input type="submit" value="Eliminar licencia" class="btn btn-danger"></th>
+				<th></th>
+			</tr>
+		</thead>
+	<tbody id="tbody">
 	{% for alumno in alumnos %}
 		<tr>
-			<td><input type="checkbox" id="{{ alumno.__GET('id_alumno') }}" name="usuario{{ alumno.__GET('id_alumno') }}"</td>
+			<td><input id="{{ alumno.__GET('id_alumno') }}" type="checkbox" name="usuario{{ alumno.__GET('id_alumno') }}"></td>
 			<td>{{ alumno.__GET('nombre') }}</td>
 			<td>{{ alumno.__GET('primer_apellido') }} {{ alumno.__GET('segundo_apellido') }}</td>
 			<td>
 				<input type="submit" value="Más informacion" class="btn btn-info">
-				<input type="submit" value="Agregar licencia" class="btn btn-success">
-				<input type="submit" value="Eliminar licencia" class="btn btn-danger">
 			</td>
 		</tr>
 	{% endfor %}
